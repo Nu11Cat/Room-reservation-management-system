74315835b241b87c6b035bb8c23387983b8071aa
Nu11Cat <2111867383@qq.com>
2025-04-16 19:05:50 +0800

    上传任务三 任务五

---
+++ documents/l/shape/Circle.java
@@ -0,0 +1,19 @@
+package shape;
+
+public class Circle extends Shape implements Drawable {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("绘制一个半径为" + radius + "的圆。");
+    }
+}
